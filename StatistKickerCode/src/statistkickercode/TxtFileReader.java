f85e23789c8fc879c5fb60b9a5a9022aaf072bf5
BrennerT <torben.brenner@googlemail.com>
2016-06-06 13:32:43 +0200

    TxtFileReader edited

--- StatistKickerCode/src/statistkickercode/TxtFileReader.java
+++ StatistKickerCode/src/statistkickercode/TxtFileReader.java
@@ -5,11 +5,43 @@
  */
 package statistkickercode;
 
+import java.io.File;
+
 /**
  *
  * @author Lukas
  */
 public class TxtFileReader {
+    
+        
+    File file;
+
+    public File getFile() {
+        this.file = new File("C:");
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+    
+    
+    
+    public void leseMetaDaten(){
+        
+    }
+    
+    public void leseNormal(){
+        
+    }
+    
+    public void leseTor(){
+        
+    }
+    
+    public Spieldaten readTxtFile(){
+        return null;    
+    }
     
     public boolean check(){
         return true;
